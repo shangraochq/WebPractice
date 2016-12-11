b94ffa4e65e1da8564f4b15b055f84874e4ccd94
chq <shangraochq@gmail.com>
2016-12-11 21:55:56 +0800

    add li zoom

--- 带运动的图片轮播/myMove.js
+++ 带运动的图片轮播/myMove.js
@@ -21,29 +21,36 @@ function getStyle ( obj, attr ) { return obj.currentStyle?obj.currentStyle[attr]
 
 
 //缓冲运动，封装了图片淡入淡出
-function move(obj,attr,target,endFn){
+function move(obj,json,endFn){
             
             /*speed = parseInt(getStyle(obj,attr))>target?-speed:speed;//判断速度的±*/
             clearInterval(obj.timer);
             obj.timer = setInterval(function(){
-                var currentVal = 0;
-                if (attr == 'opacity') {
-                    currentVal = parseInt(parseFloat(getStyle(obj,attr)*100));
+                    var stop = true;
+                    for(var attr in json){
+                    var currentVal = 0;
+                    if (attr == 'opacity') {
+                        currentVal = parseInt(parseFloat(getStyle(obj,attr)*100));
+                    }
+                    else{
+                       currentVal= parseInt(getStyle(obj,attr)); 
+                    }
+                    var speed = (json[attr] - currentVal)/8
+                    speed = speed>0 ? Math.ceil(speed) : Math.floor(speed);
+                    currentVal += speed;
+                    if (currentVal != json[attr]) {
+                        stop = false;
+                    }
+                    if (attr == 'opacity') {
+                        obj.style.filter = 'alpha(opacity:' + currentVal + ')';
+                        obj.style.opacity = currentVal/100;
+                    }
+                    else{
+                        obj.style[attr] = currentVal + "px";
+                    }
                 }
-                else{
-                   currentVal= parseInt(getStyle(obj,attr)); 
-                }
-                var speed = (target - currentVal)/8
-                speed = speed>0 ? Math.ceil(speed) : Math.floor(speed);
-                currentVal += speed;
-                if (attr == 'opacity') {
-                    obj.style.filter = 'alpha(opacity:' + currentVal + ')';
-                    obj.style.opacity = currentVal/100;
-                }
-                else{
-                    obj.style[attr] = currentVal + "px";
-                }
-                if (currentVal == target) {
+
+                if (stop) {
                     clearInterval(obj.timer);
                     endFn&&endFn();//判断是否有结束的回调函数
                 }
